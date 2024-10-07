9bb737e106a02e18c363a9f515531be94dfde310
Kristýna Bláhová <kristynablahova1234@gmail.com>
2024-10-07 19:06:26 +0200

    prvni

---
+++ script.js
@@ -0,0 +1,4 @@
+const jmeno = prompt("Zadejte sve jmeno a prijmeni", "Kvetoslav Vonavy")
+const vek = prompt("Zadejte svuj vek", 67)
+
+document.body.innerHTML += "<p>" + "jmeno" + ", věk: " + "vek" + "</p>"
